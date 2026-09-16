0d0920af6c92c9b6c2ff051876ace8a5810a03f2
meapps-it <maurizio.enardu@gmail.com>
2026-09-16 19:05:35 +0200

    Bump Turni Operai to v4

--- TurniOperai/app/build.gradle.kts
+++ TurniOperai/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.meapps.turnioperai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
     }
 
     buildTypes {
