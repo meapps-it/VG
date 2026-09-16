d0ddd0f96e121a6b082adeff31ac7dbee5b597ee
meapps-it <maurizio.enardu@gmail.com>
2026-09-16 08:44:27 +0200

    Allow Material3 experimental API in Turni Operai build

--- TurniOperai/app/build.gradle.kts
+++ TurniOperai/app/build.gradle.kts
@@ -27,7 +27,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs += "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
+    }
     buildFeatures { compose = true }
 }
 
