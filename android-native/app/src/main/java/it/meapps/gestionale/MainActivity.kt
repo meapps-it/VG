f5852ef08114ae756b0342dd73923bde98952cac
meapps-it <maurizio.enardu@gmail.com>
2026-09-25 00:23:41 +0200

    fix: import system theme detector

--- android-native/app/src/main/java/it/meapps/gestionale/MainActivity.kt
+++ android-native/app/src/main/java/it/meapps/gestionale/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
